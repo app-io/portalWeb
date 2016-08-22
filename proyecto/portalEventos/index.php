b2e58856f411314c37b3956a6ca452fd382a5d26
anibal <anibaltoro.a@gmail.com>
2016-08-21 22:34:53 -0300

    Index Prueba

--- proyecto/portalEventos/index.php
+++ proyecto/portalEventos/index.php
@@ -1,1 +1,10 @@
-<?php
+<html>
+<head>
+<title>Portal Eventos - Appio</title>
+</head>
+<body>
+	En Mantencion. <br>
+	Pero te podemos indicar la fecha: <br>
+	<?php echo date('Y-m-d'); ?>
+</body>
+</html>
