051c9f2d9cbea37b275f86ab21710e6d3f4fd437
silla <aeadf>
2016-08-22 22:32:45 -0400

    Guapos

---
+++ proyecto/portalEventos/PruebaLog.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Portal Eventos - Appio</title>
+</head>
+<body>
+
+<form>
+     Prueba Ricuras. <br>
+	Que suculento : <br>
+<input type="text" name="Ricuras"><br>
+</form>
+
+	<?php    $asdasd = "Prueba asd";    
+	echo $asdasd;
+	?>
+</body>
+</html>
